072ff43676f05ebf9447df5b4d0a78e45bc50727
Deathmax <death@deathmax.tk>
2011-12-04 13:54:53 +0800

    Reimplement item SetDefaults hooks.

--- Hooks/ItemHooks.cs
+++ Hooks/ItemHooks.cs
@@ -6,6 +6,7 @@ namespace Hooks
 	{
 		public static event SetDefaultsD<Item, int> SetDefaultsInt;
 		public static event SetDefaultsD<Item, string> SetDefaultsString;
+        public static event SetDefaultsD<Item, int> NetDefaults;
 		public static void OnSetDefaultsInt(ref int itemtype, Item item)
 		{
 			if (ItemHooks.SetDefaultsInt == null)
@@ -34,5 +35,20 @@ namespace Hooks
 			ItemHooks.SetDefaultsString(setDefaultsEventArgs);
 			itemname = setDefaultsEventArgs.Info;
 		}
+        public static void OnNetDefaults(ref int nettype, Item item)
+        {
+            if (ItemHooks.NetDefaults == null)
+            {
+                return;
+            }
+            SetDefaultsEventArgs<Item, int> setDefaultsEventArgs = new SetDefaultsEventArgs<Item, int>
+            {
+                Object = item,
+                Info = nettype
+            };
+            ItemHooks.NetDefaults(setDefaultsEventArgs);
+            nettype = setDefaultsEventArgs.Info;
+        }
+
 	}
 }
