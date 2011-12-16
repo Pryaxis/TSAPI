a72f7f729fc1fbe59e3096dba58b3786c37a32da
Deathmax <death@deathmax.tk>
2011-12-16 08:16:22 +0800

    Fix REST on TShock

--- Terraria/Netplay.cs
+++ Terraria/Netplay.cs
@@ -13,8 +13,8 @@ namespace Terraria
 		public static ServerSock[] serverSock = new ServerSock[256];
 		public static ClientSock clientSock = new ClientSock();
 		public static TcpListener tcpListener;
-		public static IPAddress serverListenIP;
-		public static IPAddress serverIP;
+        public static IPAddress serverListenIP = IPAddress.Any;
+        public static IPAddress serverIP = IPAddress.Any;
 		public static int serverPort = 7777;
 		public static bool disconnect = false;
 		public static string password = "";
@@ -291,8 +291,7 @@ namespace Terraria
 				WorldGen.genRand = new Random((int)DateTime.Now.Ticks);
 			}
 			Main.myPlayer = 255;
-			Netplay.serverIP = IPAddress.Any;
-			Netplay.serverListenIP = Netplay.serverIP;
+            Netplay.serverIP = IPAddress.Any;
 			Main.menuMode = 14;
 			Main.statusText = "Starting server...";
 			Main.netMode = 2;
