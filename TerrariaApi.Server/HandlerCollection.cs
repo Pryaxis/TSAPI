541d5ffd6c5ea018f016a3623ee5dd38cbe2e04c
CoderCow <CoderCow@gmx.de>
2013-08-06 19:43:14 +0200

    Fixed spaces -> tabs.

--- TerrariaApi.Server/HandlerCollection.cs
+++ TerrariaApi.Server/HandlerCollection.cs
@@ -5,122 +5,122 @@ using System.Diagnostics;
 
 namespace TerrariaApi.Server
 {
-  public class HandlerCollection<ArgsType>: IEnumerable<HandlerRegistration<ArgsType>> where ArgsType: EventArgs
-  {
-    private readonly List<HandlerRegistration<ArgsType>> registrations;
-    public string hookName { get; private set; }
+	public class HandlerCollection<ArgsType>: IEnumerable<HandlerRegistration<ArgsType>> where ArgsType: EventArgs
+	{
+		private readonly List<HandlerRegistration<ArgsType>> registrations;
+		public string hookName { get; private set; }
 
-    internal HandlerCollection(string hookName)
-    {
-      if (string.IsNullOrWhiteSpace(hookName))
-        throw new ArgumentException("Invalid hook name.", "hookName");
+		internal HandlerCollection(string hookName)
+		{
+			if (string.IsNullOrWhiteSpace(hookName))
+				throw new ArgumentException("Invalid hook name.", "hookName");
 
-      this.registrations = new List<HandlerRegistration<ArgsType>>();
-      this.hookName = hookName;
-    }
+			this.registrations = new List<HandlerRegistration<ArgsType>>();
+			this.hookName = hookName;
+		}
 
-    public void Register(TerrariaPlugin registrator, HookHandler<ArgsType> handler, int priority)
-    {
-      if (registrator == null)
-        throw new ArgumentNullException("registrator");
-      if (handler == null)
-        throw new ArgumentNullException("handler");
+		public void Register(TerrariaPlugin registrator, HookHandler<ArgsType> handler, int priority)
+		{
+			if (registrator == null)
+				throw new ArgumentNullException("registrator");
+			if (handler == null)
+				throw new ArgumentNullException("handler");
 
-      var newRegistration = new HandlerRegistration<ArgsType>
-      {
-        Registrator = registrator,
-        Handler = handler,
-        Priority = priority
-      };
+			var newRegistration = new HandlerRegistration<ArgsType>
+			{
+				Registrator = registrator,
+				Handler = handler,
+				Priority = priority
+			};
 
-      // Highest priority goes up in the list, first registered wins if priority equals.
-      for (int i = 0; i < this.registrations.Count + 1; i++)
-      {
-        if (i == this.registrations.Count)
-        {
-          this.registrations.Add(newRegistration);
-          break;
-        }
+			// Highest priority goes up in the list, first registered wins if priority equals.
+			for (int i = 0; i < this.registrations.Count + 1; i++)
+			{
+				if (i == this.registrations.Count)
+				{
+					this.registrations.Add(newRegistration);
+					break;
+				}
 
-        int itemPriority = this.registrations[i].Priority;
-        if (itemPriority < priority)
-        {
-          this.registrations.Insert(i, newRegistration);
-          break;
-        }
-      }
-    }
+				int itemPriority = this.registrations[i].Priority;
+				if (itemPriority < priority)
+				{
+					this.registrations.Insert(i, newRegistration);
+					break;
+				}
+			}
+		}
 
 		public void Register(TerrariaPlugin registrator, HookHandler<ArgsType> handler)
 		{
 			this.Register(registrator, handler, registrator.Order);
 		}
 
-    public bool Deregister(TerrariaPlugin registrator, HookHandler<ArgsType> handler)
-    {
-      if (registrator == null)
-        throw new ArgumentNullException("registrator");
-      if (handler == null)
-        throw new ArgumentNullException("handler");
+		public bool Deregister(TerrariaPlugin registrator, HookHandler<ArgsType> handler)
+		{
+			if (registrator == null)
+				throw new ArgumentNullException("registrator");
+			if (handler == null)
+				throw new ArgumentNullException("handler");
 
-      var registration = new HandlerRegistration<ArgsType>
-      {
-        Registrator = registrator,
-        Handler = handler
-      };
-      int registrationIndex = this.registrations.IndexOf(registration);
-      if (registrationIndex == -1)
-        return false;
+			var registration = new HandlerRegistration<ArgsType>
+			{
+				Registrator = registrator,
+				Handler = handler
+			};
+			int registrationIndex = this.registrations.IndexOf(registration);
+			if (registrationIndex == -1)
+				return false;
 
-      this.registrations.RemoveAt(registrationIndex);
-      return true;
-    }
+			this.registrations.RemoveAt(registrationIndex);
+			return true;
+		}
 
-    public void Invoke(ArgsType args)
-    {
-      foreach (var registration in this.registrations) {
-        try
-        {
-          if (ServerApi.Profiler == null)
-          {
-            registration.Handler(args);
-          }
-          else
-          {
-            Stopwatch watch = new Stopwatch();
+		public void Invoke(ArgsType args)
+		{
+			foreach (var registration in this.registrations) {
+				try
+				{
+					if (ServerApi.Profiler == null)
+					{
+						registration.Handler(args);
+					}
+					else
+					{
+						Stopwatch watch = new Stopwatch();
 
-            watch.Start();
-            try
-            {
-              registration.Handler(args);
-            }
-            finally
-            {
-              watch.Stop();
-              ServerApi.Profiler.InputPluginHandlerTime(registration.Registrator, hookName, watch.Elapsed);
-            }
-          }
-        }
-        catch (Exception ex)
-        {
-          ServerApi.LogWriter.ServerWriteLine(string.Format(
-            "Plugin \"{0}\" has had an unhandled exception thrown by one of its {1} handlers: \n{2}",
-            registration.Registrator.Name, hookName, ex), TraceLevel.Warning);
+						watch.Start();
+						try
+						{
+							registration.Handler(args);
+						}
+						finally
+						{
+							watch.Stop();
+							ServerApi.Profiler.InputPluginHandlerTime(registration.Registrator, hookName, watch.Elapsed);
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					ServerApi.LogWriter.ServerWriteLine(string.Format(
+						"Plugin \"{0}\" has had an unhandled exception thrown by one of its {1} handlers: \n{2}",
+						registration.Registrator.Name, hookName, ex), TraceLevel.Warning);
 
-          if (ServerApi.Profiler != null)
-            ServerApi.Profiler.InputPluginHandlerExceptionThrown(registration.Registrator, hookName, ex);
-        }
-      }
-    }
+					if (ServerApi.Profiler != null)
+						ServerApi.Profiler.InputPluginHandlerExceptionThrown(registration.Registrator, hookName, ex);
+				}
+			}
+		}
 
-    IEnumerator<HandlerRegistration<ArgsType>> IEnumerable<HandlerRegistration<ArgsType>>.GetEnumerator()
-    {
-      return this.registrations.GetEnumerator();
-    }
+		IEnumerator<HandlerRegistration<ArgsType>> IEnumerable<HandlerRegistration<ArgsType>>.GetEnumerator()
+		{
+			return this.registrations.GetEnumerator();
+		}
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-      return this.registrations.GetEnumerator();
-    }
-  }
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return this.registrations.GetEnumerator();
+		}
+	}
 }
--- TerrariaApi.Server/HandlerRegistration.cs
+++ TerrariaApi.Server/HandlerRegistration.cs
@@ -3,36 +3,36 @@ using System.Linq;
 
 namespace TerrariaApi.Server
 {
-  internal struct HandlerRegistration<ArgsType> where ArgsType: EventArgs
-  {
-    public TerrariaPlugin Registrator { get; set; }
-    public HookHandler<ArgsType> Handler { get; set; }
-    public int Priority { get; set; }
+	internal struct HandlerRegistration<ArgsType> where ArgsType: EventArgs
+	{
+		public TerrariaPlugin Registrator { get; set; }
+		public HookHandler<ArgsType> Handler { get; set; }
+		public int Priority { get; set; }
+		
+		public override int GetHashCode()
+		{
+			return this.Registrator.GetHashCode() ^ this.Handler.GetHashCode() ^ this.Priority;
+		}
 
-    public override int GetHashCode()
-    {
-      return this.Registrator.GetHashCode() ^ this.Handler.GetHashCode() ^ this.Priority;
-    }
+		public override bool Equals(object obj)
+		{
+			if (!(obj is HandlerRegistration<ArgsType>))
+				return false;
 
-    public override bool Equals(object obj)
-    {
-      if (!(obj is HandlerRegistration<ArgsType>))
-        return false;
+			HandlerRegistration<ArgsType> other = (HandlerRegistration<ArgsType>)obj;
+			return (
+				this.Registrator == other.Registrator &&
+				this.Handler.Equals(other.Handler));
+		}
 
-      HandlerRegistration<ArgsType> other = (HandlerRegistration<ArgsType>)obj;
-      return (
-        this.Registrator == other.Registrator &&
-        this.Handler.Equals(other.Handler));
-    }
+		public static bool operator ==(HandlerRegistration<ArgsType> a, HandlerRegistration<ArgsType> b)
+		{
+			return a.Equals(b);
+		}
 
-    public static bool operator ==(HandlerRegistration<ArgsType> a, HandlerRegistration<ArgsType> b)
-    {
-      return a.Equals(b);
-    }
-
-    public static bool operator !=(HandlerRegistration<ArgsType> a, HandlerRegistration<ArgsType> b)
-    {
-      return !a.Equals(b);
-    }
-  }
+		public static bool operator !=(HandlerRegistration<ArgsType> a, HandlerRegistration<ArgsType> b)
+		{
+			return !a.Equals(b);
+		}
+	}
 }
