9cc7a0abc205fdf3ab941a2b8958267dd03ab3cf
Tyler Watson <tyler@tw.id.au>
2015-07-08 18:21:30 +1000

    Sendq nearly perfect, still overlaps regions in some cases

--- Terraria/Net/Sockets/SendQueue.cs
+++ Terraria/Net/Sockets/SendQueue.cs
@@ -10,55 +10,76 @@ using Terraria.Net.Sockets.EventArgs;
 namespace Terraria.Net.Sockets
 {
 
-    public class SendQueue : IDisposable
-    {
-        protected const int kSendQueueInitialBufferSize = 262144;
-	    protected volatile bool threadCancelled;
-        protected byte[] sendBuffer;
-        protected readonly object syncLock = new object();
-        protected BlockingCollection<ArraySegment<byte>> bufferSegments;
-	    protected BlockingCollection<ArraySegment<byte>> sendQueue;
-        protected Thread sendThread;
-	    protected RemoteClient client;
+	public class SendQueue : IDisposable
+	{
+		protected const int kSendQueueInitialBufferSize = 262144;
+		protected volatile bool threadCancelled;
+		protected byte[] sendBuffer;
+		protected readonly object syncLock = new object();
+		protected List<ArraySegment<byte>> bufferSegments;
+		protected BlockingCollection<ArraySegment<byte>> sendQueue;
+		protected Thread sendThread;
+		protected RemoteClient client;
 
-        public event EventHandler<WriteFailedEventArgs> WriteFailed;
-        public event EventHandler BufferGrow;
+		public event EventHandler<WriteFailedEventArgs> WriteFailed;
+		public event EventHandler BufferGrow;
 
 		public int QueueLength { get { return bufferSegments.Count; } }
 
-        public SendQueue(RemoteClient client)
-        {
-	        this.client = client;
-			bufferSegments = new BlockingCollection<ArraySegment<byte>>();
+		public SendQueue(RemoteClient client)
+		{
+			this.client = client;
+			bufferSegments = new List<ArraySegment<byte>>();
 			sendQueue = new BlockingCollection<ArraySegment<byte>>();
-            sendBuffer = new byte[kSendQueueInitialBufferSize];
+			sendBuffer = new byte[kSendQueueInitialBufferSize];
 			sendThread = new Thread(WriteThread);
-	        sendThread.Name = client.Id + " - Network I/O Thread - " + client.Name;
-	        sendThread.Start();
+			sendThread.Name = client.Id + " - Network I/O Thread - " + client.Name;
+			sendThread.Start();
 
 			Console.WriteLine("IO thread - slot {0}", client.Id);
-        }
+		}
 
-	    protected void WriteThread()
-	    {
-		    ArraySegment<byte> segment;
+		protected void WriteThread()
+		{
+			ArraySegment<byte> segment;
+			SocketError? socketError = null;
+			while (true)
+			{
+				if (threadCancelled == true)
+				{
+					break;
+				}
 
-		    while (true)
-		    {
-			    if (threadCancelled == true)
-			    {
-				    break;
-			    }
+				if (sendQueue.TryTake(out segment, TimeSpan.FromMilliseconds(100)) == false)
+				{
+					continue;
+				}
 
-			    if (sendQueue.TryTake(out segment, TimeSpan.FromMilliseconds(100)) == false)
-			    {
-				    continue;
-			    }
-
-			    (client.Socket as TcpSocket)._connection.GetStream()
-				    .BeginWrite(segment.Array, segment.Offset, segment.Count, WriteCallback, segment);
-		    }
-	    }
+				try
+				{
+					(client.Socket as TcpSocket)._connection.GetStream()
+						.BeginWrite(segment.Array, segment.Offset, segment.Count, WriteCallback, segment);
+				}
+				catch (IOException ioe)
+				{
+					socketError = (ioe.InnerException as SocketException).SocketErrorCode;
+				}
+				catch (SocketException se)
+				{
+					socketError = se.SocketErrorCode;
+				}
+				catch (Exception)
+				{
+				}
+				finally
+				{
+					if (WriteFailed != null && socketError.HasValue == true)
+					{
+						WriteFailed(this, new WriteFailedEventArgs() { ErrorCode = socketError.Value });
+					}
+				}
+			}
+		}
 
 		private void WriteCallback(IAsyncResult ar)
 		{
@@ -79,105 +100,135 @@ namespace Terraria.Net.Sockets
 			catch (Exception)
 			{
 			}
-
-			if (WriteFailed != null && socketError.HasValue == true)
+			finally
 			{
-				WriteFailed(this, new WriteFailedEventArgs() { ErrorCode = socketError.Value });
+				if (WriteFailed != null && socketError.HasValue == true)
+				{
+					WriteFailed(this, new WriteFailedEventArgs() { ErrorCode = socketError.Value });
+				}
+
+				__segment_release_internal((ArraySegment<byte>)ar.AsyncState);
 			}
 		}
 
-	    public void Enqueue(ArraySegment<byte> segment)
-	    {
-		    Console.WriteLine("slot {2} Enqueued {0} bytes @ {1}", segment.Count, segment.Offset, client.Id);
-		    sendQueue.TryAdd(segment);
-	    }
+		public void Enqueue(ArraySegment<byte> segment)
+		{
+			//Console.WriteLine("slot {0} Enqueued segment {1} @ {2}", client.Id, segment, sendQueue.Count);
+			sendQueue.TryAdd(segment);
+		}
 
-        protected void Grow(int sizeDelta)
-        {
-            try
-            {
-                Array.Resize<byte>(ref sendBuffer, sizeDelta);
-            }
-            catch (OutOfMemoryException)
-            {
-                Console.WriteLine("Grow failed: Out of memory asking for {0} bytes", sizeDelta);
-            }
-        }
+		protected void Grow(int sizeDelta)
+		{
+			try
+			{
+				Array.Resize<byte>(ref sendBuffer, sizeDelta);
+			}
+			catch (OutOfMemoryException)
+			{
+				Console.WriteLine("Grow failed: Out of memory asking for {0} bytes", sizeDelta);
+			}
+		}
 
-        internal ArraySegment<byte> __segment_lock_internal(int offset, int size)
-        {
-            ArraySegment<byte> newSegment = new ArraySegment<byte>(sendBuffer, offset, size);
+		internal ArraySegment<byte> __segment_lock_internal(int offset, int size)
+		{
+			ArraySegment<byte> newSegment = new ArraySegment<byte>(sendBuffer, offset, size);
 
-	        if (bufferSegments.TryAdd(newSegment, 100) == false)
-	        {
-		        throw new Exception(string.Format("Could not reserve buffer region at {0},{1}", offset, size));
-	        }
+			__segment_lock_internal(newSegment);
+	
+			return newSegment;
+		}
 
-            return newSegment;
-        }
+		internal void __segment_lock_internal(ArraySegment<byte> segment)
+		{
+			lock (syncLock)
+			{
+				bufferSegments.Add(segment);
+			}
+				
+			//Console.WriteLine("queue {0}: locked {1} bytes @ {2}/{3}", client.Id, segment.Count, segment.Offset, bufferSegments.Count);
+		}
 
-	    public ArraySegment<byte> LockSegment(short size)
-	    {
+		internal void __segment_release_internal(ArraySegment<byte> segment)
+		{
+			lock (syncLock)
+			{
+				bufferSegments.Remove(segment);
+			}
+		}
+
+		public ArraySegment<byte> LockSegment(short size)
+		{
 			int previousSegmentEnd = 0;
 
 			/*
 			 * If there are no segments in the buffer, it is completely
 			 * free.
 			 */
-			if (bufferSegments.Count == 0)
-		    {
-			    return __segment_lock_internal(0, size);
-		    }
-		    
-		    foreach (var bufferSegment in bufferSegments)
-		    {
-			    /*
-                 * If this is the first segment and the distance between
-                 * here and the start of the array is big enough to fit the
-                 * segment then fit it at position 0.
-                 */
-			    if (previousSegmentEnd == 0 && bufferSegment.Offset < size)
-			    {
-				    return __segment_lock_internal(0, size);
-			    }
+			lock (syncLock)
+			{
+				if (bufferSegments.Count == 0)
+				{
+					return __segment_lock_internal(0, size);
+				}
 
-			    if (bufferSegment.Offset - previousSegmentEnd > size)
-			    {
-				    return __segment_lock_internal(previousSegmentEnd, size);
-			    }
 
-			    previousSegmentEnd += bufferSegment.Count;
-		    }
+				foreach (var bufferSegment in bufferSegments)
+				{
+					/*
+					 * If this is the first segment and the distance between
+					 * here and the start of the array is big enough to fit the
+					 * segment then fit it at position 0.
+					 */
+					if (bufferSegment == bufferSegments.First() && bufferSegment.Offset >= size)
+					{
+						return __segment_lock_internal(0, size);
+					}
 
-		    //The buffer must get bigger to accommodate
-			Grow(size - previousSegmentEnd);
-		    return __segment_lock_internal(previousSegmentEnd, size);
-	    }
+					if (bufferSegment.Offset - previousSegmentEnd > size)
+					{
+						return __segment_lock_internal(previousSegmentEnd, size);
+					}
 
-	    public BinaryWriter SegmentWriter(ArraySegment<byte> segment)
-	    {
-		    return new BinaryWriter(new MemoryStream(segment.Array, segment.Offset, segment.Count, true));
-	    }
+					previousSegmentEnd += bufferSegment.Offset + bufferSegment.Count;
+				}
 
-	    public BinaryWriter SegmentWriter(short size)
-	    {
-		    ArraySegment<byte> segment = LockSegment(size);
-		    return SegmentWriter(segment);
-	    }
+				if (sendBuffer.Length - previousSegmentEnd >= size)
+				{
+					return __segment_lock_internal(previousSegmentEnd, size);
+				}
 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+				//The buffer must get bigger to accommodate
+				Grow(size - previousSegmentEnd);
+				return __segment_lock_internal(previousSegmentEnd, size);
+			}
+		}
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-	            threadCancelled = true;
-				bufferSegments.Dispose();
-            }
-        }
-    }
+		public BinaryWriter SegmentWriter(ArraySegment<byte> segment)
+		{
+			return new BinaryWriter(new MemoryStream(segment.Array, segment.Offset, segment.Count, true));
+		}
+
+		public BinaryWriter SegmentWriter(short size)
+		{
+			ArraySegment<byte> segment = LockSegment(size);
+			return SegmentWriter(segment);
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				threadCancelled = true;
+				sendThread.Join();
+				bufferSegments = null;
+				sendBuffer = null;
+			}
+		}
+	}
 }
