94fa1ca6536c1f124bc4934e49db96dc05600eb0
Lucas Nicodemus <shank@shanked.me>
2017-04-19 14:41:22 -0600

    Update SSC mod to 1.3.5

--- TShock.Modifications.SSC/SSC.cs
+++ TShock.Modifications.SSC/SSC.cs
@@ -20,7 +20,7 @@ namespace TShock.Modifications.SSC
 		public override string Description => "Adding server side character support...";
 		public override void Run()
 		{
-			var sendData = this.Method(() => Terraria.NetMessage.SendDataDirect(0, 0, 0, "", 0, 0, 0, 0, 0, 0, 0));
+			var sendData = this.Method(() => Terraria.NetMessage.SendDataDirect(0, 0, 0, Terraria.Localization.NetworkText.Empty, 0, 0, 0, 0, 0, 0, 0));
 
 			//find the offset where we want to inject the Terraria.Main.ServerSideCharacter variable
 			var downedClown = sendData.Body.Instructions.Single( //expect one for the signature this modification is based around.
