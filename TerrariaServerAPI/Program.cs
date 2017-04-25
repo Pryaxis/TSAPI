2f7a79edd7b09288d2f3ad56bf765f77a0297cff
Tyler Watson <tyler@tw.id.au>
2017-04-25 13:56:17 +1000

    Revert "Added git stamps to TSAPI"
    This reverts commit 20166fac5a4ee45c97088cec4cc6293f2fa1e5d0.

--- TerrariaServerAPI/Program.cs
+++ TerrariaServerAPI/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
-using System.Reflection;
 using Terraria.ID;
 using TerrariaApi.Server;
 
@@ -54,14 +53,6 @@ namespace OTAPI.Shims.TShock
 
 		static void StartServer(string[] args)
 		{
-			var gitInfo = Attribute.GetCustomAttribute(Assembly.GetEntryAssembly(), typeof(AssemblyInformationalVersionAttribute)) as AssemblyInformationalVersionAttribute;
-
-			if (gitInfo != null )
-			{
-				Console.Write(" Mintaka v");
-				Console.WriteLine(gitInfo?.InformationalVersion);
-			} 
-
 			if (args.Any(x => x == "-skipassemblyload"))
 			{
 				Terraria.Main.SkipAssemblyLoad = true;
