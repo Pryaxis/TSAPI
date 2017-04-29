a586e24a1cf52220877bad213f1137ff4577e5ca
ZIT WANG <mymisting@gmail.com>
2017-04-29 13:40:08 +0800

    Add check to make sure that the .cctor only contains what we currently expect it to

--- TShock.Modifications.Platform/PlatformModification.cs
+++ TShock.Modifications.Platform/PlatformModification.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Mono.Cecil.Cil;
 using OTAPI.Patcher.Engine.Modification;
@@ -17,16 +18,21 @@ namespace Mintaka.Modifications.Platform
 			"OTAPI, Version=1.3.5.3, Culture=neutral, PublicKeyToken=null"
 		};
 
-		public override string Description => "Enforcing OTAPI to load right platform";
+		public override string Description => "Enforcing OTAPI to load right platform...";
 
 		public override void Run()
 		{
 			ReLogic.OS.Platform temp = null;
 			var changePlatformMethodDefinition = SourceDefinition.MainModule.Import(Method(() => Callbacks.PlatformConstructorCallback.ChangePlatform(ref temp)));
 
-			var type = Type<ReLogic.OS.Platform>();
-			var cctor = type.Methods.Single(m => m.Name == ".cctor");
-			var field = type.Fields.Single(f => f.IsStatic && f.Name == "Current");
+			var field = Field(() => ReLogic.OS.Platform.Current);
+			var cctor = Type<ReLogic.OS.Platform>().Methods.Single(m => m.Name == ".cctor");
+			var instructions = cctor.Body.Instructions;
+
+			if (instructions?.Count != 3 || instructions[1].OpCode != OpCodes.Stsfld || instructions[1].Operand != field)
+			{
+				throw new NotSupportedException("Could not patch Platform..cctor()");
+			}
 
 			var processor = cctor.Body.GetILProcessor();
 
