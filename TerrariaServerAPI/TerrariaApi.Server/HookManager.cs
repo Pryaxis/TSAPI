8e6a6ae72df18d2e92c9a69fdc45246ff25f4bc6
Chris <2648373+QuiCM@users.noreply.github.com>
2018-11-07 14:59:54 +1030

    0-length patch from master this time

--- TerrariaServerAPI/TerrariaApi.Server/HookManager.cs
+++ TerrariaServerAPI/TerrariaApi.Server/HookManager.cs
@@ -371,6 +371,20 @@ namespace TerrariaApi.Server
 		{
 			if (Main.netMode == 2)
 			{
+				// A critical server crash/slow-down bug was exploited in which a 0-length
+				// packet is sent, causing all NetGetData handlers to throw exceptions.
+				// Because a packet's header is 2 bytes of length + 1 byte of packet type,
+				// all packets must contain at least 3 bytes.
+				// Ideally this check should occur in an OTAPI modification.
+				if (length < 1)
+				{
+					RemoteClient currentClient = Netplay.Clients[buffer.whoAmI];
+					string name = String.IsNullOrWhiteSpace(currentClient.Name) ? "Unknown" : currentClient.Name;
+					ServerApi.LogWriter.ServerWriteLine("Disconnecting " + name + " (" + currentClient.Socket.GetRemoteAddress() + ") for attempted packet length crash/DoS attempt.", TraceLevel.Error);
+					Netplay.Clients[buffer.whoAmI].PendingTermination = true;
+				}
+
+
 				// A critical server crash/corruption bug was reported by @bartico6 on GitHub.
 				// If a packet length comes in at extreme values, the server can enter infinite loops, deadlock, and corrupt the world.
 				// As a result, we take the following action: disconnect the player and log the attempt as soon as we can.
