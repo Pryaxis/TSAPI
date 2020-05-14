4875376c54824ecf8d7f3c653c04672879ecc50b
鱼鱼 <axeel@qq.com>
2020-05-14 16:53:54 +0800

    Revert "Remove rest"
    This reverts commit 57b112ed6658659baf52ff6d77649300edf79ac5.

---
+++ TerrariaServerAPI/TerrariaApi.Server/Logging/ILogWriter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace TerrariaApi.Server
+{
+	/// <summary>
+	///   Implements a server log writer. Methods of this implementation are generally expected to be thread safe. Note that
+	///   the plugin which has registered the log writer should never dispose it during deinitialization or disposing, let the
+	///   Server API call the detach method instead.
+	/// </summary>
+	public interface ILogWriter
+	{
+		/// <summary>
+		///   Gets the name of the log writer implementation.
+		/// </summary>
+		string Name { get; }
+
+		/// <summary>
+		///   Called before this log writer is detached, that is when the server is shutting down or the log writer is replaced by 
+		///   another log writer. This method should never be called by a plugin.
+		/// </summary>
+		void Detach();
+
+		/// <summary>
+		///   Expects the log writer to write a line produced by the server itself. This method should never be called by a plugin.
+		/// </summary>
+		/// <param name="message">
+		///   The message to write.
+		/// </param>
+		/// <param name="kind">
+		///   The kind of message.
+		/// </param>
+		void ServerWriteLine(string message, TraceLevel kind);
+
+		/// <summary>
+		///   Expects the log writer to write a line produced by a plugin.
+		/// </summary>
+		/// <param name="plugin">
+		///   The plugin writing the line.
+		/// </param>
+		/// <param name="message">
+		///   The message to write.
+		/// </param>
+		/// <param name="kind">
+		///   The kind of message.
+		/// </param>
+		void PluginWriteLine(TerrariaPlugin plugin, string message, TraceLevel kind);
+	}
+}
---
+++ TerrariaServerAPI/TerrariaApi.Server/Logging/LogWriterManager.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TerrariaApi.Server
+{
+	public sealed class LogWriterManager
+	{
+		internal ServerLogWriter DefaultLogWriter
+		{
+			get; 
+			private set;
+		}
+		internal ILogWriter WrappedLogWriter
+		{
+			get; 
+			private set;
+		}
+		public string LogWriterName
+		{
+			get { return this.WrappedLogWriter.Name; }
+		}
+
+		internal LogWriterManager()
+		{
+			this.DefaultLogWriter = new ServerLogWriter();
+			this.WrappedLogWriter = this.DefaultLogWriter;
+		}
+
+		public void Attach(ILogWriter newLogWriter)
+		{
+			if (newLogWriter == null)
+				throw new ArgumentNullException("newLogWriter");
+			if (newLogWriter == this.WrappedLogWriter)
+				return;
+
+			ILogWriter prevLogWriter = this.WrappedLogWriter;
+			Exception detachException = null;
+			if (this.WrappedLogWriter != null)
+			{
+				try
+				{
+					prevLogWriter.Detach();
+				}
+				catch (Exception ex)
+				{
+					detachException = ex;
+				}
+
+				this.WrappedLogWriter.ServerWriteLine(
+					string.Format("Log writer \"{0}\" is being detached.", this.LogWriterName), TraceLevel.Verbose);
+			}
+
+			this.WrappedLogWriter = newLogWriter;
+
+			this.WrappedLogWriter.ServerWriteLine(
+				string.Format("Log writer \"{0}\" has been attached.", this.LogWriterName), TraceLevel.Verbose);
+
+			if (detachException != null)
+				this.ServerWriteLine(
+					string.Format("Log writer \"{0}\" had thrown an unexpected exception:\n{1}", prevLogWriter.Name, detachException), 
+					TraceLevel.Error);
+		}
+
+		internal void Deatch()
+		{
+			if (this.WrappedLogWriter == null)
+				return;
+
+			try
+			{
+				this.WrappedLogWriter.Detach();
+			}
+			catch (Exception ex)
+			{
+				DefaultLogWriter.ServerWriteLine(
+					string.Format("Log writer \"{0}\" has thrown an unexpected exception:\n{1}", this.LogWriterName, ex), TraceLevel.Error);
+			}
+
+			this.WrappedLogWriter = null;
+		}
+
+		internal void ServerWriteLine(string message, TraceLevel kind)
+		{
+			try
+			{
+				this.WrappedLogWriter.ServerWriteLine(message, kind);
+			}
+			catch (Exception ex)
+			{
+				this.DefaultLogWriter.ServerWriteLine(string.Format(
+					"The attached log writer \"{0}\" has thrown an unexpected exception:\n{1}", this.LogWriterName, ex),
+					TraceLevel.Error);
+			}
+		}
+
+		public void PluginWriteLine(TerrariaPlugin plugin, string message, TraceLevel kind)
+		{
+			try
+			{
+				this.WrappedLogWriter.PluginWriteLine(plugin, message, kind);
+			}
+			catch (Exception ex)
+			{
+				DefaultLogWriter.ServerWriteLine(string.Format(
+					"The attached log writer \"{0}\" has thrown an unexpected exception:\n{1}", this.LogWriterName, ex),
+					TraceLevel.Error);
+			}
+		}
+	}
+}
