4c2075fbdd7e9b17dd96d6495fe9f491a2af0bbb
Lucas Nicodemus <shank@shanked.me>
2022-03-01 19:29:20 -0800

    Update to OTAPi 2.0.0.50 for Terraria 1.4.3.6

--- TShock.Modifications.Explosives/Explosives.cs
+++ TShock.Modifications.Explosives/Explosives.cs
@@ -8,7 +8,7 @@ namespace Mintaka.Modifications.Explosives
 	{
 		public override System.Collections.Generic.IEnumerable<string> AssemblyTargets => new[]
 		{
-			"OTAPI, Version=1.4.3.5, Culture=neutral, PublicKeyToken=null"
+			"OTAPI, Version=1.4.3.6, Culture=neutral, PublicKeyToken=null"
 		};
 		public override string Description => "Adding ItemID.Sets.Explosives...";
 		public override void Run()
